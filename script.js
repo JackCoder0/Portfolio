3153fed431fc5f1ac2c6bc209d0c0bacc8d60d1f
JackCoder0 <jacksonxcontato13@gmail.com>
2024-01-15 23:28:22 -0300

    Menu Responsivo

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+// Função de Redirecionamento de Página dos Projetos
+
 function ProjetoBr() {
     window.location.href = 'project-br.html'
 }
 function ProjetoPP() {
     window.location.href = 'project-pp.html'
 }
+
+
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('open-menu')
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('open-menu')
+})
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('open-menu')
+})
