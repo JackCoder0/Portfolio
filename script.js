d2316e24161a30550c5fc7eebe4f164a59806b6d
JackCoder0 <jacksonxcontato13@gmail.com>
2024-01-17 20:38:23 -0300

    Função CV e WPP

--- script.js
+++ script.js
@@ -24,3 +24,34 @@ menu.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     menu.classList.remove('open-menu')
 })
+
+// Função do botão de download do CV
+
+function downloadFile(filePath, fileName) {
+    var link = document.createElement('a');
+    link.href = filePath;
+    link.download = fileName;
+    link.click();
+}
+
+document.getElementById('resume').addEventListener('click',function() {
+    var filePath = 'assets/pdf/Curriculo.pdf';
+    var fileName = 'cv-jackson.pdf';
+    downloadFile(filePath, fileName);
+});
+
+document.getElementById('resume-about').addEventListener('click',function() {
+    var filePath = 'assets/pdf/Curriculo.pdf';
+    var fileName = 'cv-jackson.pdf';
+    downloadFile(filePath, fileName);
+});
+
+// Funcção para mandar mensagem pelo WhatsApp
+
+document.getElementById('start').addEventListener('click', function() {
+    var numeroTelefone = '5511952909057';
+    var mensagem = 'Oi tudo bem? Vim pelo seu portfolio!';
+    var link = 'https://api.whatsapp.com/send?phone=' + numeroTelefone + '&text=' + encodeURIComponent(mensagem);
+
+    window.location.href = link;
+})
